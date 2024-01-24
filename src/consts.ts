d6beea4296c0c133e07982c46257a8522ae74ef3
imac <imac@192.168.1.13>
2024-01-24 18:22:29 +0000

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Declarez vos pain 🥖';
+export const SITE_DESCRIPTION = 'bienvenue dans declarez vos pain!';
